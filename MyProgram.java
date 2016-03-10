96394f89383c88428635cdccbfab0adbd841bf6c
Cesar Rodriguez <cesar89.rodriguez@gmail.com>
2016-03-10 17:06:43 -0600

    Hey guys, I added three lines in our java program, they can validate something somewhere

--- MyProgram.java
+++ MyProgram.java
@@ -5,6 +5,9 @@ public class MyProgram{
 	public static void main(String args[])
 	{
 		System.out.println("This is a Java program written by Cesar");
+		System.out.println("Line 1");
+		System.out.println("Line 2");
+		System.out.println("Line 3");
 		System.exit(0);
 	}	
 }
